50d96e11597d0bd7cca1a4c6c70ffc5a9c7f43d3
felipe <pipega2000@hotmail.com>
2022-08-08 09:18:51 -0500

    Interfaces Created

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,4 +2,30 @@
 
 export interface postI{
     
+    postId: number|null,
+    title: string,
+    content: string,
+    numberOfLikes: number|null,
+    userLikesPost: userLikeI[]|null,
+    comments: commentI|null
+
+
 }
+
+export interface commentI{
+
+    commentId: number|null,
+    commentContent: string,
+    numberOfLikes: number|null,
+    post: postI,
+    userLikesComment: userLikeI|null,
+
+
+}
+
+
+
+export interface userLikeI{
+    
+}
+
